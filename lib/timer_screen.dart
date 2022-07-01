4cecb240a98b7938d094e901b75d6ac02f3d3b26
Michael Karp <mkprogrammer15@gmail.com>
2022-07-01 23:33:45 +0200

    added shake widget

--- lib/timer_screen.dart
+++ lib/timer_screen.dart
@@ -1,13 +1,12 @@
-import 'dart:math';
-
 import 'package:adesso_timer/timer_service/timer_service.dart';
 import 'package:adesso_timer/utils/background_gradient.dart';
 import 'package:adesso_timer/utils/style_constants.dart';
 import 'package:adesso_timer/widgets/button_timer.dart';
+import 'package:adesso_timer/widgets/shake_widget.dart';
 import 'package:adesso_timer/widgets/time_input.dart';
 import 'package:adesso_timer/widgets/time_painter.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
 
 class TimerScreen extends StatefulWidget {
   @override
@@ -18,7 +17,7 @@ class _TimerScreenState extends State<TimerScreen> with TickerProviderStateMixin
   late final AnimationController controller;
   late final Animation<double> progressAnimation;
   bool startButtonIsActive = false;
-
+  final shakeKey = GlobalKey<ShakeWidgetState>();
   final TextEditingController timeInput = TextEditingController();
 
   @override
@@ -52,98 +51,111 @@ class _TimerScreenState extends State<TimerScreen> with TickerProviderStateMixin
       body: BackGroundGradient(
         child: ValueListenableBuilder(
           valueListenable: TimerService.counter,
-          builder: (context, _, __) => SingleChildScrollView(
-            child: SizedBox(
-              height: size.height,
-              width: size.width,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(
-                    height: 140,
-                    width: 100,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(border: Border.all(width: 2, color: inkDark), borderRadius: BorderRadius.circular(90)),
-                    width: 250,
-                    height: 50,
-                    child: TimeInput(timeInput: timeInput, counter: TimerService.counter),
-                  ),
-                  const SizedBox(
-                    height: 50,
-                  ),
-                  Stack(
-                    alignment: AlignmentDirectional.center,
-                    children: [
-                      SizedBox(
-                        width: 200,
-                        height: 200,
-                        child: TimePainter(
-                          animation: progressAnimation,
-                          progressColor: progressAnimation.value > 0.6 ? Colors.red : apple,
-                        ),
-                      ),
-                      Center(
-                        child: Container(
-                          width: 160,
-                          height: 160,
-                          decoration: const BoxDecoration(color: inkBrightest, shape: BoxShape.circle),
-                        ),
-                      ),
-                      Text(
-                        'Seconds : ${TimerService.counter.value}',
-                        style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                      ),
-                    ],
-                  ),
-                  Expanded(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          builder: (context, _, __) {
+            if (TimerService.counter.value == 0) {
+              shakeKey.currentState?.shake();
+            }
+            return SingleChildScrollView(
+              child: SizedBox(
+                height: size.height,
+                width: size.width,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 140,
+                      width: 100,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(border: Border.all(width: 2, color: inkDark), borderRadius: BorderRadius.circular(90)),
+                      width: 250,
+                      height: 50,
+                      child: TimeInput(timeInput: timeInput, counter: TimerService.counter),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Stack(
+                      alignment: AlignmentDirectional.center,
                       children: [
-                        TimerButton(
-                          timeInput: timeInput,
-                          onPressed: startButtonIsActive
-                              ? () {}
-                              : () {
-                                  startButtonIsActive = true;
-                                  final userValue = int.parse(timeInput.text.isEmpty ? '10' : timeInput.text);
-                                  if (TimerService.counter.value != userValue) {
-                                    return;
-                                  } else {
-                                    TimerService.startTimer(userValue);
-                                    setState(() {
-                                      controller
-                                        ..duration = Duration(seconds: TimerService.counter.value)
-                                        ..forward();
-                                    });
-                                  }
-                                },
-                          backGroundColor: startButtonIsActive ? blueyGrey : apple,
-                          buttonName: 'Start',
+                        ShakeWidget(
+                          key: shakeKey,
+                          shakeOffset: 3,
+                          shakeCount: 4,
+                          shakeDuration: const Duration(seconds: 1),
+                          child: SizedBox(
+                            width: 200,
+                            height: 200,
+                            child: TimePainter(
+                              animation: progressAnimation,
+                              progressColor: progressAnimation.value > 0.6 ? pinkyRed : apple,
+                            ),
+                          ),
                         ),
-                        const SizedBox(
-                          width: 20,
+                        Center(
+                          child: Container(
+                            width: 160,
+                            height: 160,
+                            decoration: const BoxDecoration(color: inkBrightest, shape: BoxShape.circle),
+                          ),
                         ),
-                        TimerButton(
-                          buttonName: 'Stop',
-                          backGroundColor: cherry,
-                          timeInput: timeInput,
-                          onPressed: () {
-                            startButtonIsActive = false;
-                            TimerService.stopTimer();
-                            TimerService.resetTimer();
-                            timeInput.clear();
-                            controller.reset();
-                          },
-                        )
+                        Text(
+                          'Seconds : ${TimerService.counter.value}',
+                          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
                       ],
                     ),
-                  )
-                ],
+                    Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          TimerButton(
+                            timeInput: timeInput,
+                            onPressed: startButtonIsActive
+                                ? () {}
+                                : () {
+                                    startButtonIsActive = true;
+                                    final userValue = int.parse(timeInput.text.isEmpty ? '10' : timeInput.text);
+                                    if (TimerService.counter.value != userValue) {
+                                      return;
+                                    } else {
+                                      HapticFeedback.heavyImpact();
+                                      TimerService.startTimer(userValue);
+                                      setState(() {
+                                        controller
+                                          ..duration = Duration(seconds: TimerService.counter.value)
+                                          ..forward();
+                                      });
+                                    }
+                                  },
+                            backGroundColor: startButtonIsActive ? blueyGrey : apple,
+                            buttonName: 'Start',
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          TimerButton(
+                            buttonName: 'Stop',
+                            backGroundColor: cherry,
+                            timeInput: timeInput,
+                            onPressed: () {
+                              HapticFeedback.heavyImpact();
+                              startButtonIsActive = false;
+                              TimerService.stopTimer();
+                              TimerService.resetTimer();
+                              timeInput.clear();
+                              controller.reset();
+                            },
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-          ),
+            );
+          },
         ),
       ),
     );
